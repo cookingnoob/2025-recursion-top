3ac87d74d0f84bf6e36833c963f78c80d7c5d79f
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-08-14 10:16:33 -0600

    add comments with pseudocode for the algorithm instructions

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,8 +1,15 @@
 def merge_sort(array)
+  
   middle = array.length / 2
+
+  # sort left half
   left = array[0..middle - 1]
+
+  # sort right half
   right = array[middle..-1]
-  p "left #{left} right #{right}"
+
+  # merge
+
 end
 
 merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
