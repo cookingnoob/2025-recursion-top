8e2b25fb7eaf97b547420b188b2b6ea6f62aa88d
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-08-13 19:12:19 -0600

    made the recursive function work with the dispatcher strategy of reducing the problem from the iterative function and handling base cases

--- fibonacci.rb
+++ fibonacci.rb
@@ -8,8 +8,10 @@ def fibs_iterative(number)
     else
       answer.push(answer[i - 2] + answer[i - 1])
     end
+
     i += 1
   end
+
   answer
 end
 
@@ -17,13 +19,15 @@ fibs_iterative(8)
 
 def fibs_rec(n)
   if n == 0
-    return 0
+    return [0]
   elsif n == 1
-    return 1
+    return [1]
   else
-    'recursive magic'
+   ans = fibs_iterative(n - 1) 
+   ans + [ans[n - 2] + ans[n - 3]]
   end
-  
 end
 
 p fibs_rec(8)
+
+# [0, 1, 1, 2, 3, 5, 8, 13]
